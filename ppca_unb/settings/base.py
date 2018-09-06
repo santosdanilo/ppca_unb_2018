9a6db7e1b6bce097d46a5d39fbd50e2eb9b39359
Danilo Santos <dan-desenvolvimento@hotmail.com>
2018-09-06 12:46:24 -0300

    Initial settings

--- ppca_unb/settings/base.py
+++ ppca_unb/settings/base.py
@@ -151,7 +151,7 @@ MEDIA_URL = '/media/'
 
 # Wagtail settings
 
-WAGTAIL_SITE_NAME = "ppca_unb"
+WAGTAIL_SITE_NAME = "PPCA UnB"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
